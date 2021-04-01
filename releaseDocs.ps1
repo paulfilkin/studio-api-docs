721e3fb80e9621641531ae1e72f3d47e3f8bae62
dwatson <dwatson@sdl.com>
2021-04-01 18:15:03 +0100

    make it more like powershell

--- releaseDocs.ps1
+++ releaseDocs.ps1
@@ -15,19 +15,19 @@ write-host "Removing temporary doc directory $TEMP_REPO_DIR"
 Remove-Item $TEMP_REPO_DIR -Force -Recurse -ErrorAction Ignore
 mkdir $TEMP_REPO_DIR
 
-write-host "Cloning the repo ${remote_repo} with the gh-pages branch"
-git clone ${remote_repo} --branch gh-pages $TEMP_REPO_DIR
+write-host "Cloning the repo $remote_repo with the gh-pages branch"
+git clone $remote_repo --branch gh-pages $TEMP_REPO_DIR
 
 write-host "Clear repo directory"
 Set-Location $TEMP_REPO_DIR
 git rm -r *
 
 write-host "Copy documentation into the repo"
-Copy-Item -r $SOURCE_DIR/_site/* .
+Copy-Item "$SOURCE_DIR\_site\*.*" . -Recurse
 
 write-host "Push the new docs to the remote branch"
 git config --local user.email "github-actions[bot]@users.noreply.sdl.com"
 git config --local user.name "github-actions[bot]"
 git add . -A
 git commit -m "Update generated documentation"
-git push "${remote_repo}" HEAD:gh-pages
+git push "$remote_repo" HEAD:gh-pages
